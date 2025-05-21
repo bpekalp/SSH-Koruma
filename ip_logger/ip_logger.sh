3e04c218347a144eb1fe68cb1933346916882c0b
Güven Barış PEKALP <b.pekalp@gmail.com>
2025-05-21 16:01:01 +0300

    Cron Job için betiğin güncellenmesi

--- ip_logger/ip_logger.sh
+++ ip_logger/ip_logger.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Tarih
 NOW=$(date '+%Y-%m-%d %H:%M:%S')
-
-# Engellenen IP'ler
 BANNED_IPS=$(sudo fail2ban-client status sshd 2>/dev/null | grep 'Banned IP list:' | cut -d ':' -f2 | xargs)
+RECORD_FILE="/home/sistemodev/SSH-Koruma/ip_logger/ip_ban_records.log"
 
-# Kayıt dosyası
-RECORD_FILE="/var/log/ip_ban_records.log"
+# Log kaydı
+echo "[$NOW] Banned IPs: $BANNED_IPS" >>"$RECORD_FILE"
 
-# Kaydı dosyaya yazma (manuel inceleme için)
-echo "[$NOW] Banned IPs: $BANNED_IPS" >>$RECORD_FILE]
-
-# Kaydı bildirim olarak göndermek
+# Mail bildirimi
 if [ -n "$BANNED_IPS" ]; then
-    echo -e "[$NOW] Tarihli Engelli IP'ler Listesi: $BANNED_IPS" | mail -s "[SSH Koruma] Fail2Ban Engellenen IP'ler" -c b.pekalp@gmail.com zzehrakr48@gmail.com
+    echo -e "[$NOW] Tarihli Engelli IP'ler Listesi: $BANNED_IPS" |
+        mail -s "[SSH Koruma] Fail2Ban Engellenen IP'ler" zzehrakr48@gmail.com
+    echo -e "[$NOW] Tarihli Engelli IP'ler Listesi: $BANNED_IPS" |
+        mail -s "[SSH Koruma] Fail2Ban Engellenen IP'ler" b.pekalp@gmail.com
 fi
