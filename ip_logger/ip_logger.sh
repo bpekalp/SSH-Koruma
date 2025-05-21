c3bde8e78fd4d4a147b2568fc0844a2edafb8e8d
Güven Barış PEKALP <b.pekalp@gmail.com>
2025-05-21 08:43:35 +0300

    Fail2Ban ile engellenen IP'lerin kaydının alınması

---
+++ ip_logger/ip_logger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Tarih
+NOW=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Engellenen IP'ler
+BANNED_IPS=$(sudo fail2ban-client status sshd 2>/dev/null | grep 'Banned IP list:' | cut -d ':' -f2 | xargs)
+
+# Kayıt dosyası
+RECORD_FILE="/var/log/ip_ban_records.log"
+
+# Kaydı dosyaya yazma (manuel inceleme için)
+echo "[$NOW] Banned IPs: $BANNED_IPS" >> $RECORD_FILE]
+
+# Kaydı bildirim olarak göndermek
+if [ -n "$BANNED_IPS" ]; then
+    echo -e "[$NOW] Tarihli Engelli IP'ler Listesi: $BANNED_IPS" | mail -s "[SSH Koruma] Fail2Ban Engellenen IP'ler" -c b.pekalp@gmail.com zzehrakr48@gmail.com
+fi
